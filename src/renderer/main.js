4fb6fcdc0b553d07cd636d73c3e6971c2aa22f8e
Rafael <rafaelleru95103@gmail.com>
2017-08-30 22:16:22 +0200

    added ipc file

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import App from './App'
 import store from '../store'
+import ipc from './ipc'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
@@ -9,6 +10,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   store,
+  ipc,
   components: { App },
   template: '<App/>'
 }).$mount('#app')

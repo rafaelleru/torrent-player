80e181417422103a613d006d289bf60090ffe6da
Rafael <rafaelleru95103@gmail.com>
2017-08-31 19:00:40 +0200

    new play ipc

--- src/renderer/ipc.js
+++ src/renderer/ipc.js
@@ -4,3 +4,9 @@ const {ipcRenderer} = require('electron')
 ipcRenderer.on('updateSongs', (event, args) => {
   store.dispatch('addSongs', args)
 })
+
+ipcRenderer.on('canPlay', (event, args) => {
+  console.log('play file' + args)
+  // store.commit('updateTorrentId', args[0])
+  store.commit('updateSongIndex', args)
+})

b1d649dc5da142104a3e4d0209f85fe09e4010b4
Rafael <rafaelleru95103@gmail.com>
2017-09-01 18:03:04 +0200

    deleted test songs

--- src/store/modules/playlist.js
+++ src/store/modules/playlist.js
@@ -1,36 +1,5 @@
 const state = {
-  songs: [
-    {
-      title: '01 - Cigarrito',
-      torrent: 'sadlkjfañlksjfdñlksajdfñlkajsdf',
-      index: 1,
-      duration: '3:15'
-    },
-    {
-      title: '02 - Tras la barra',
-      torrent: 'sadlkjfañlksjfdñlksajdfñlkajsdf',
-      index: 1,
-      duration: '3:15'
-    },
-    {
-      title: '03 - Alucinante',
-      torrent: 'sadlkjfañlksjfdñlksajdfñlkajsdf',
-      index: 1,
-      duration: '3:15'
-    },
-    {
-      title: '03 - Si la tocas otra vez',
-      torrent: 'sadlkjfañlksjfdñlksajdfñlkajsdf',
-      index: 1,
-      duration: '3:15'
-    },
-    {
-      title: '03 - Un abecedario sin letras',
-      torrent: 'sadlkjfañlksjfdñlksajdfñlkajsdf',
-      index: 1,
-      duration: '3:15'
-    }
-  ]
+  songs: []
 }
 
 const getters = {

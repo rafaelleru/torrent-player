fc6b2a7676dad6f93ae7f9ec23b293d472d7b837
Rafael <rafaelleru95103@gmail.com>
2017-09-01 17:59:22 +0200

    remove not audio files from playlist. Add ipcEvent for audio enede.

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -11,13 +11,15 @@ ipcMain.on('addMagnet', (event, arg) => {
   client.add(arg, function (torrent) {
     var files = []
     torrent.files.forEach(function (file) {
-      files.push({
-        title: file.name,
-        torrent: torrent.infoHash,
-        index: torrent.files.indexOf(file),
-        duration: '--:--',
-        playing: 'false'
-      })
+      if (file.name.indexOf('.mp3') !== -1 || file.name.indexOf('.ogg') !== -1) {
+        files.push({
+          title: file.name,
+          torrent: torrent.infoHash,
+          index: torrent.files.indexOf(file),
+          duration: '--:--',
+          playing: 'false'
+        })
+      }
     })
     event.sender.send('updateSongs', files)
   })
@@ -28,13 +30,29 @@ ipcMain.on('requestPlay', (event, args) => {
     server = client.get(args[0]).createServer()
     server.listen(9999)
     torrentId = args[0]
-    event.sender.send('canPlay', [args[1], args[2]])
-  } else if (torrentId !== args[1]) {
+    event.sender.send('canPlay', args)
+  } else if (torrentId !== args[0]) {
     server.close()
     server = client.get(args[0]).createServer()
     server.listen(9999)
-    event.sender.send('canPlay', [args[1], args[2]])
+    event.sender.send('canPlay', args)
   } else {
-    event.sender.send('canPlay', [args[1], args[2]])
+    event.sender.send('canPlay', args)
+  }
+})
+
+ipcMain.on('playEnded', (event, args) => {
+  console.log('end' + args)
+  var title
+  if (torrentId !== args[0]) {
+    server.close()
+    server = client.get(args[0]).createServer()
+    server.listen(9999)
+    title = client.get(args[0]).files[args[1]].name
+    event.sender.send('canPlay', args.concat([title]))
+  } else {
+    title = client.get(args[0]).files[args[1]].name
+    console.log(args.concat([title]))
+    event.sender.send('canPlay', args.concat([title]))
   }
 })

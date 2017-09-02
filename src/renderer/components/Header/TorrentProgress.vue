78fa689847a605421043aa92ea636de8678612a2
Rafael <rafaelleru95103@gmail.com>
2017-09-02 17:02:28 +0200

    new component showing torrent status

---
+++ src/renderer/components/Header/TorrentProgress.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="progess" class="wrapper">
+    <div>
+      <i style="font-size: small;">{{ title }}</i>
+    </div>
+    <div>
+      <i style="font-size: x-small;">{{ hash }}</i>
+    </div>
+    <md-progress md-progress="progress"></md-progress>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'torrent-progress',
+    props: ['title', 'hash', 'progress']
+  }
+</script>
+
+<style>
+</style>

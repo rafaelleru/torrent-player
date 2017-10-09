c85f02359fec8681bfbb11cc8add75897fb549d4
Rafael Leyva Ruiz <rafaelleru95103@gmail.com>
2017-10-09 19:03:56 +0200

    add flac extension

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -6,12 +6,15 @@ const client = new WebTorrent()
 // const async = require('async')
 var server = null
 var torrentId = null
+var filesToPlay = []
+var currentFileOnPlay = null
 
 ipcMain.on('addMagnet', (event, arg) => {
   client.add(arg, function (torrent) {
     var files = []
     torrent.files.forEach(function (file) {
-      if (file.name.indexOf('.mp3') !== -1 || file.name.indexOf('.ogg') !== -1) {
+      if (file.name.indexOf('.mp3') !== -1 || file.name.indexOf('.ogg') !== -1 ||
+          file.name.indexOf('.flac') !== -1) {
         files.push({
           title: file.name.replace('.mp3', ''),
           torrent: torrent.infoHash,
@@ -19,8 +22,14 @@ ipcMain.on('addMagnet', (event, arg) => {
           duration: '--:--',
           playing: 'false'
         })
+        filesToPlay.push({
+          title: file.name.replace('.mp3', ''),
+          torrent: torrent.infoHash,
+          index: torrent.files.indexOf(file)
+        })
       }
     })
+
     event.sender.send('updateSongs', files)
   })
 })
@@ -45,16 +54,28 @@ ipcMain.on('playEnded', (event, args) => {
   console.log('end' + args)
   var title
   var newIndex = args[1] + 1
-  if (torrentId !== args[0]) {
-    server.close()
-    server = client.get(args[0]).createServer()
-    server.listen(9999)
-    title = client.get(args[0]).files[args[1]].name
-    event.sender.send('canPlay', args.concat([title]))
-  } else {
+  // if (torrentId !== args[0]) {
+  //   server.close()
+  //   server = client.get(args[0]).createServer()
+  //   server.listen(9999)
+  //   title = client.get(args[0]).files[args[1]].name
+  //   event.sender.send('canPlay', args.concat([title]))
+  // } else {
+  //   title = client.get(args[0]).files[args[1] + 1].name
+  //   console.log(args.concat([title]))
+  //   event.sender.send('canPlay', [args[0], newIndex, title])
+  // }
+
+  if (currentFileOnPlay.torrent === filesToPlay[filesToPlay.indexOf(currentFileOnPlay) + 1]) {
     title = client.get(args[0]).files[args[1] + 1].name
-    console.log(args.concat([title]))
     event.sender.send('canPlay', [args[0], newIndex, title])
+  } else {
+    currentFileOnPlay = filesToPlay[filesToPlay.indexOf(currentFileOnPlay) + 1]
+    torrentId = currentFileOnPlay.torrent
+    server.close()
+    server = client.get(torrentId).createServer()
+    server.listen(9999)
+    event.sender.send('canPlay', [torrentId, currentFileOnPlay.index, currentFileOnPlay.title])
   }
 })
 
